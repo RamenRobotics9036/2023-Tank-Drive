23304b6142962701d6f4a8339ebd703885276fbc
RamenRobotics9036 <ramenrobotics0@gmail.com>
2023-01-28 10:38:19 -0800

    Cleaned code and added better controls.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,15 +8,11 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
-// import edu.wpi.first.wpilibj.Compressor;
-// import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-// import edu.wpi.first.wpilibj.PneumaticHub;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -28,30 +24,27 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
+  private final TalonSRX m_armWinchMotor = new TalonSRX(20);
+
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+  private PneumaticHub PHub = new PneumaticHub();
+  private DoubleSolenoid m_solenoid;
+
   private final MotorController m_leftMotor = new CANSparkMax(10, MotorType.kBrushless);
   private final MotorController m_rightMotor = new CANSparkMax(12, MotorType.kBrushless);
-  private final TalonSRX m_armWinchMotor = new TalonSRX(20);
-  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
-  private DoubleSolenoid m_solenoid;
   private DifferentialDrive m_tankDrive;
-  private boolean arcadeDrive = true;
   private XboxController m_controller;
-  private PneumaticHub PHub = new PneumaticHub();
-  
-
-  public static final String m_armWinchGainKey = "ArmWinchGain";
-  // Debug Arm Winch Value
-  private static final double m_armWinchGainDefault = 0.8;
-  private static double m_armWinchGainValue = m_armWinchGainDefault;
 
   @Override
   public void robotInit() {
+    initRobotPreferences();
+    m_compressor.enableDigital();
     m_controller = new XboxController(0);
+
     m_rightMotor.setInverted(true);
     m_leftMotor.setInverted(true);
     m_tankDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
     m_tankDrive.setMaxOutput(0.1);
-    m_compressor.enableDigital();
 
     CameraServer.startAutomaticCapture();
   }
@@ -62,57 +55,33 @@ public class Robot extends TimedRobot {
     m_solenoid = PHub.makeDoubleSolenoid(0, 1);
   }
 
-  private void initRobotPreferences()
-  {
-    // Init robot preferences if they don't already exist in flash memory
-    if (!Preferences.containsKey(m_armWinchGainKey)) {
-      Preferences.setDouble(m_armWinchGainKey, m_armWinchGainDefault);
-    }
-
-    m_armWinchGainValue = Preferences.getDouble(m_armWinchGainKey, m_armWinchGainDefault);
-    if (m_armWinchGainValue < 0 || m_armWinchGainValue > 1)
-    {
-      m_armWinchGainValue = m_armWinchGainDefault;
-    }
+  private void initRobotPreferences() {
   }
 
   @Override
   public void teleopPeriodic() {
-    if (m_controller.getAButtonReleased()) {
-      arcadeDrive = !arcadeDrive;
-      System.out.println("DRIVE MODE SWITCHED TO " + arcadeDrive);
-    }
+    m_tankDrive.tankDrive(m_controller.getLeftY(), -m_controller.getRightY(), true);
 
-    if (arcadeDrive == true) {
-      m_tankDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getLeftX(), true);
-    } else if (arcadeDrive == false) {
-      m_tankDrive.tankDrive(m_controller.getLeftY(), -m_controller.getRightY(), true);
-    }
-
-    // X-button turns motor on forward and Y button for reverse
-    if (m_controller.getXButton()) {
-      System.out.println("X BUTTON PRESSED");
-      m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, 1);
-    } else if (m_controller.getYButton()) {
-        System.out.println("Y BUTTON PRESSED");
-        m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, -1);
+    // Right Trigger turns motor on forward and Left Trigger for reverse
+    if (m_controller.getRightTriggerAxis() > 0) {
+      m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, m_controller.getRightTriggerAxis());
+      System.out.println("RIGHT TRIGGER PRESSED | OUTPUT SET TO " + m_armWinchMotor.getMotorOutputPercent());
+    } else if (m_controller.getLeftTriggerAxis() > 0) {
+        m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, m_controller.getLeftTriggerAxis());
+        System.out.println("LEFT TRIGGER PRESSED | OUTPUT SET TO " + m_armWinchMotor.getMotorOutputPercent());
     } else {
       m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, 0);
     }
-    // Comment
 
+    // Left Bumper sets solenoid to forward and Left Bumper sets solenoid to reverse
     if (m_controller.getRightBumperPressed()){
-      // Activate Solenoid
-      System.out.println("Right bumper pressed");
-      System.out.println("Solenoid Channels :" + m_solenoid.get());
       m_solenoid.set(DoubleSolenoid.Value.kForward);
+      System.out.println("RIGHT BUMPER PRESSED | OUTPUT SET TO " + m_solenoid.get());
     } else if (m_controller.getLeftBumperPressed()){
-        // Deactivate Solenoid
-        m_solenoid.toggle();
-        System.out.println("Left bumper pressed");
-        System.out.println("Solenoid Channels :" + m_solenoid.get());
+        m_solenoid.set(DoubleSolenoid.Value.kReverse);
+        System.out.println("LEFT BUMPER PRESSED | OUTPUT SET TO " + m_solenoid.get());
     } else {
-        //m_solenoid.set(DoubleSolenoid.Value.kOff);
+        // m_solenoid.set(DoubleSolenoid.Value.kOff);
     }
   }
 }
