cb31d769c9336b9f63ad113eb99ee5912281d5cb
RamenRobotics9036 <ramenrobotics0@gmail.com>
2023-02-06 17:47:03 -0800

    Make driving controls with 2 devices.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -17,11 +16,12 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController; 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
@@ -42,9 +42,12 @@ public class Robot extends TimedRobot {
   private final CANSparkMax m_leftMotor2 = new CANSparkMax(11, MotorType.kBrushless);
   private final CANSparkMax m_rightMotor1 = new CANSparkMax(12, MotorType.kBrushless);
   private final CANSparkMax m_rightMotor2 = new CANSparkMax(13, MotorType.kBrushless);
+
   private final MotorControllerGroup m_leftMotor = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
   private final MotorControllerGroup m_rightMotor = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
   private DifferentialDrive m_tankDrive;
+
+  private Joystick m_joystick;
   private XboxController m_controller;
 
   private RelativeEncoder m_leftMotorEncoder;
@@ -60,6 +63,7 @@ public class Robot extends TimedRobot {
     m_leftMotor2.setIdleMode(IdleMode.kBrake);
     m_rightMotor1.setIdleMode(IdleMode.kBrake);
     m_leftMotor2.setIdleMode(IdleMode.kBrake);
+    m_joystick = new Joystick(1);
 
     m_controller = new XboxController(0);
     m_rightMotor.setInverted(true);
@@ -71,7 +75,7 @@ public class Robot extends TimedRobot {
     // m_rightArmSolenoid = m_pneumaticHub.makeDoubleSolenoid(2, 3);
 
     m_tankDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_tankDrive.setMaxOutput(0.15);
+    m_tankDrive.setMaxOutput(0.2);
 
     m_rightMotorEncoder = m_leftMotor1.getEncoder();
     m_leftMotorEncoder = m_rightMotor1.getEncoder();
@@ -102,14 +106,14 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    m_tankDrive.tankDrive(-m_controller.getLeftY(), -m_controller.getRightY(), true);
+    m_tankDrive.arcadeDrive(-m_joystick.getY(), -m_joystick.getX(), true);
 
     // Right Trigger turns motor on forward and Left Trigger for reverse
     if (m_controller.getRightTriggerAxis() > 0) {
-      m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, Math.pow(m_controller.getRightTriggerAxis(), 2));
+      m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, -Math.pow(m_controller.getRightTriggerAxis(), 2));
       System.out.println("RIGHT TRIGGER PRESSED | OUTPUT SET TO " + m_armWinchMotor.getMotorOutputPercent());
     } else if (m_controller.getLeftTriggerAxis() > 0) {
-        m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, -Math.pow(m_controller.getLeftTriggerAxis(), 2));
+        m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, Math.pow(m_controller.getLeftTriggerAxis(), 2));
         System.out.println("LEFT TRIGGER PRESSED | OUTPUT SET TO " + m_armWinchMotor.getMotorOutputPercent());
     } else {
       m_armWinchMotor.set(TalonSRXControlMode.PercentOutput, 0);
@@ -131,15 +135,17 @@ public class Robot extends TimedRobot {
         // m_rightArmSolenoid.set(DoubleSolenoid.Value.kOff);
     }
 
-    if (m_controller.getAButton()) {
-      m_armExtensionMotor.set(VictorSPXControlMode.PercentOutput, 0.8);
-      System.out.println("A BUTTON PRESSED | OUTPUT SET TO " + m_armExtensionMotor.getMotorOutputPercent());
-    } else if (m_controller.getYButton()) {
-        m_armExtensionMotor.set(VictorSPXControlMode.PercentOutput, -0.8);
-        System.out.println("Y BUTTON PRESSED | OUTPUT SET TO " + m_armExtensionMotor.getMotorOutputPercent());
+    if (m_controller.getLeftY() > 0.1 || m_controller.getLeftY() < 0.1) {
+      m_armExtensionMotor.set(VictorSPXControlMode.PercentOutput, -m_controller.getLeftY());
+      System.out.println("LEFT JOYSTICK PRESSED | OUTPUT SET TO " + m_armExtensionMotor.getMotorOutputPercent());
+    } else if (m_controller.getRightY() > 0.1 || m_controller.getRightY() < 0.1) {
+        m_armExtensionMotor.set(VictorSPXControlMode.PercentOutput, -m_controller.getRightY());
+        System.out.println("RIGHT JOYSTICK PRESSED | OUTPUT SET TO " + m_armExtensionMotor.getMotorOutputPercent());
     } else {
       m_armExtensionMotor.set(VictorSPXControlMode.PercentOutput, 0);
     }
+
+    // TODO: Flip joysticks and triggers for drive team
   }
 
   @Override
