10bd70c49269b6ba32829d5e118da52323648e08
Ido <idoben2@hotmail.com>
2023-02-13 22:22:03 -0800

    Add Drivetrain simulation
    Add IDrivetrainWrapper to abstract physical and simulated drivetrains
    
    
    Add placeholders for periodic methods
    
    
    Create DrivetrainWrapperSimulation class
    
    
    Fixed a bug where dead-band for motor-arm didn't work
    
    
    IDrivetrainWrapper can now pass deadband values
    
    
    Factory to create DrivetrainWrapper
    
    
    Cleanup
    
    
    Cleanup
    
    
    Placeholder functions for relative encoders in simulation
    
    
    Fix relative encoders to show distance in meters
    
    
    Simple autonomous demo

---
+++ src/main/java/frc/robot/IDrivetrainWrapper.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public interface IDrivetrainWrapper {
+  public void setMaxOutput(double maxOutput);
+  public void setDeadband(double deadband);
+
+  public void robotPeriodic();
+  public void simulationPeriodic();
+
+  public void arcadeDrive(double xSpeed, double zRotation, boolean squareInputs);
+
+  public double getLeftRelativeDistance();
+  public double getRightRelativeDistance();
+  public void resetRelativeEncoders();
+}
+
